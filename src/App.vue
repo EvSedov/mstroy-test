5f086a556f73f98180fdd254f25dd69f453ec1a0
sedovev <sedovev@rgn.gov.ru>
2025-07-31 02:46:36 +0700

    changes: изменил иконку для удаления

--- src/App.vue
+++ src/App.vue
@@ -182,8 +182,24 @@ watch(testData, () => {
                   v-if="currentMode === TableMode.EDIT"
                   class="delete-icon"
                   @click.stop="removeDataset(file.name)"
-                  >&times;</span
                 >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="18"
+                    height="18"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="#dc3545"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="feather feather-x-circle"
+                  >
+                    <circle cx="12" cy="12" r="10" fill="#dc3545" />
+                    <line x1="15" y1="9" x2="9" y2="15" stroke="white" />
+                    <line x1="9" y1="9" x2="15" y2="15" stroke="white" />
+                  </svg>
+                </span>
               </div>
             </li>
           </ul>
@@ -401,14 +417,9 @@ watch(testData, () => {
 }
 
 .delete-icon {
-  color: #dc3545;
+  width: 18px;
+  height: 18px;
   cursor: pointer;
-  font-size: 18px;
-  font-weight: bold;
-}
-
-.delete-icon:hover {
-  color: #c82333;
 }
 
 .add-new-file-item {
